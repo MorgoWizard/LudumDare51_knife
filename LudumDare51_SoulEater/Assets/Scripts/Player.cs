3e494b1bb16c338740cc0d2af074b260551c55aa
potikot <ildusxtr@mail.ru>
2022-10-01 23:13:09 +0300

    Fix: Player script
    Add: Player attack

--- LudumDare51_SoulEater/Assets/Scripts/Player.cs
+++ LudumDare51_SoulEater/Assets/Scripts/Player.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Player : MonoBehaviour
 {
-    [SerializeField]private int souls = 0;
+    private int souls = 0;
     private int health = 10;
     private int maxHealth = 10;
 
     private float rotY = 0f;
+    private float rotX = 0f;
+
+    [SerializeField] private Transform camTransform;
 
     [SerializeField] private float speed = 5f;
     [SerializeField] private float jumpForce = 10f;
@@ -18,13 +22,14 @@ public class Player : MonoBehaviour
 
     private void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
         rb = GetComponent<Rigidbody>();
+        camTransform = Camera.main.transform;
     }
 
     private void Update()
     {
         Rotate();
-        //Move();
         Jump();
         //Attack();
     }
@@ -36,8 +41,13 @@ public class Player : MonoBehaviour
 
     private void Rotate()
     {
-        rotY += Input.GetAxis("Mouse X");
-        transform.rotation = Quaternion.Euler(0f, rotY, 0f);
+        rotX += Input.GetAxis("Mouse Y") * sensetivity;
+        rotY += Input.GetAxis("Mouse X") * sensetivity;
+
+        rotX = rotX < 75f ? rotX : 75f;
+        rotX = rotX > -75f ? rotX : -75f;
+
+        camTransform.rotation = Quaternion.Euler(-rotX, rotY, 0f);
     }
 
     private void Move()
@@ -45,22 +55,29 @@ public class Player : MonoBehaviour
         if (!isGrounded)
             return;
 
-        //rb.velocity = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        Vector3 forward = camTransform.forward;
+        Vector3 right = camTransform.right;
+        forward.y = 0f;
+        right.y = 0f;
+        forward = forward.magnitude < 1f ? forward * (1f / forward.magnitude) : forward;
+        right = right.magnitude < 1f ? right * (1f / right.magnitude) : right;
+
+
         if (Input.GetKey(KeyCode.W))
         {
-            rb.AddForce(transform.forward * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            rb.AddForce(forward * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            rb.AddForce(-transform.forward * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            rb.AddForce(-forward * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            rb.AddForce(transform.right * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            rb.AddForce(right * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            rb.AddForce(-transform.right * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+            rb.AddForce(-right * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
         }
     }
 
@@ -70,11 +87,6 @@ public class Player : MonoBehaviour
         {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
         }
-    }
-
-    private void Attack()
-    {
-
     }
 
     public void AddSoul()
---
+++ LudumDare51_SoulEater/Assets/Scripts/Sword.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator), typeof(BoxCollider))]
+public class Sword : MonoBehaviour
+{
+    private Animator animator;
+    private BoxCollider triggerCollider;
+
+    [SerializeField] private int damage = 1;
+    [SerializeField] private float cooldown = 1.5f;
+    private bool canAttack = true;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        triggerCollider = GetComponent<BoxCollider>();
+        triggerCollider.enabled = false;
+    }
+
+    private void Update()
+    {
+        Attack();
+    }
+
+    private void Attack()
+    {
+        if (!canAttack || !Input.GetMouseButtonDown(0))
+            return;
+
+        triggerCollider.enabled = true;
+        animator.SetTrigger("Attack");
+        canAttack = false;
+        StartCoroutine(AttackCooldown());
+    }
+
+    private IEnumerator AttackCooldown()
+    {
+        yield return new WaitForSeconds(cooldown);
+        canAttack = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            other.GetComponent<Enemy>().TakeDamage(damage);
+        }
+    }
+
+    public void DisableCollider() => triggerCollider.enabled = false;
+}
